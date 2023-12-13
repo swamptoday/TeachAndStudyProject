92b5fa33a97c41137a1454c4a25cc144745fa104
Марина Низова <maryna.nyzova.04@gmail.com>
2023-12-13 17:47:26 +0200

    initial

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 from unicodedata import name
 from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Curriculum, Note, User, Tutor
+from .models import Curriculum, Note, User, Tutor, Calendar, Student
 from . import db
 import json
-
+from datetime import datetime
 
 
 views = Blueprint('views', __name__)
@@ -38,11 +38,46 @@ def delete_note():
 @views.route('/calendar', methods=['GET'])
 @login_required
 def calendar():
-    return render_template("calendar.html", user=current_user)
+    if current_user.is_tutor:
+        tutor = Tutor.query.filter_by(user_id = current_user.id).first()
+        events = Calendar.query.filter_by(tutor_id = tutor.id).all()
+        formatted_events = []
+        for event in events:
+            user = User.query.filter_by(id = event.student_id).first()
+            formatted_event = {
+                'id': f'{event.id}',  # Assuming you want the ID as a 6-digit string
+                'name': 'Заняття',  # Replace this with the actual event name from your data
+                'description': "У вас заняття з учнем " + user.first_name + " " + user.last_name +" :)",
+                'date': event.date.strftime('%m/%d/%Y'),  # Format date as 'mm/dd/yyyy'
+                'type': 'event'
+            }
+        formatted_events.append(formatted_event)
+    elif current_user.is_student:
+        student = Student.query.filter_by(user_id = current_user.id).first()
+        events = Calendar.query.filter_by(student_id = student.id).all()
+        formatted_events = []
+        for event in events:
+            print(event)
+            user = User.query.filter_by(id = event.tutor_id).first()
+            formatted_event = {
+                'id': f'{event.id}',  # Assuming you want the ID as a 6-digit string
+                'name': 'Заняття',  # Replace this with the actual event name from your data
+                'description': "У вас заняття з репетитором " + user.first_name + " " + user.last_name +" :)",
+                'date': event.date.strftime('%m/%d/%Y'),  # Format date as 'mm/dd/yyyy'
+                'type': 'event'
+            }
+            formatted_events.append(formatted_event)
+       
+    
+    return render_template("calendar.html", user=current_user, events=formatted_events)
 
 @views.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
+    tutor = None
+
+    if current_user.is_tutor == True:
+        tutor = Tutor.query.filter_by(user_id = current_user.id).first()
     if request.method == 'POST':
         # Fetch the current user
         user = current_user
@@ -53,11 +88,21 @@ def profile():
         user.email = request.form.get('email')
         user.phone = request.form.get('phone')
         user.address = request.form.get('address')
+        
+        if current_user.is_tutor == True:
+            tutor.description = request.form.get('description')
+            tutor.subjects = request.form.get('subjects')
+            print(tutor.description)
+            print(request.form.get('description'))
+            db.session.commit()
+
 
         # Commit changes to the database
         db.session.commit()
+        
+        print(tutor.description)
 
-    return render_template("profile.html", user=current_user)
+    return render_template("profile.html", user=current_user, tutor = tutor)
 
 @views.route('/curriculum', methods=['GET'])
 @login_required
@@ -94,13 +139,24 @@ def curriculum_add():
             new_curriculum = Curriculum(name=name, description=description, amount=amount, level=level, topics=topics, tutor_id=tutor_id)
             db.session.add(new_curriculum)
             db.session.commit()
+            return redirect(url_for('views.curriculum'))
 
     return render_template("curriculum_add.html", user=current_user)
 
-@views.route('/tutors', methods=['GET'])
+@views.route('/tutors', methods=['GET', 'POST'])
 def tutors():
+    if request.method == 'POST':
+        date = request.form.get('date')
+        formatted_date = datetime.fromisoformat(date)
 
-        # Fetch the current user's ID
+        tutor_email = request.form.get('tutor')
+        user_tutor = User.query.filter_by(email = tutor_email).first()
+        tutor = Tutor.query.filter_by(user_id = user_tutor.id).first()
+        student = Student.query.filter_by(user_id = current_user.id).first()
+        new_calendar = Calendar(date=formatted_date, tutor_id = tutor.id, student_id = student.id)
+        db.session.add(new_calendar)
+        db.session.commit()
+    # Fetch the current user's ID
     # Find the tutor entity with the same user ID as the current user
     tutors = Tutor.query.all()
     users = User.query.filter_by(is_tutor = True).all()
@@ -128,6 +184,6 @@ def tutors():
         user_id = user_dict.pop('id')  # Remove 'id' to avoid duplication
         new_tutor_dict = {**user_dict, 'tutors': tutor_counts.get(user_id, 0), 'users': user_counts.get(user_id, 0)}
         new_tutors.append(new_tutor_dict)
-    for tutor in new_tutors:
-        print(tutor)
+    print(new_tutors)
+
     return render_template("tutors.html", user=current_user, tutors=new_tutors)
